066687b92150b8481b096229dbbf31c78846e3e0
chronixlo <tkarvonen@gmail.com>
2016-06-15 17:28:34 +0300

    tabs to spaces

--- src/roll.js
+++ src/roll.js
@@ -1,15 +1,15 @@
 function roll(args, callback) {
-	var max = parseInt(args, 10);
-	
-	if (isNaN(max)) {
-		max = 100;
-	}
-	
-	if (max < 1) {
-		callback('too low');
-	}
-	
-	callback(Math.floor(Math.random() * (max + 1)));
+    var max = parseInt(args, 10);
+    
+    if (isNaN(max)) {
+        max = 100;
+    }
+    
+    if (max < 1) {
+        callback('too low');
+    }
+    
+    callback(Math.floor(Math.random() * (max + 1)));
 }
 
 module.exports = roll;
