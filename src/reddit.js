ad06f739c54ffd4931b752a5a82fa24b381f90ac
chronixlo <tkarvonen@gmail.com>
2016-06-16 11:10:06 +0300

    reddit: fetch 5 posts and return the first non-sticky

--- src/reddit.js
+++ src/reddit.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 function reddit(args, callback) {
     request({
-        url: 'https://api.reddit.com/r/' + args + '/hot',
+        url: 'https://api.reddit.com/r/' + args + '/hot?limit=5',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
         }
@@ -13,7 +13,15 @@ function reddit(args, callback) {
         
         body = JSON.parse(body);
         
-        if (body.data) {
+        if (body.data && body.data.children.length) {
+            for (var i = 0, l = body.data.children.length; i < l; i++) {
+                if (!body.data.children[i].data.stickied) {
+                    callback('http://www.reddit.com' + body.data.children[i].data.permalink);
+                    return;
+                }
+            }
+            
+            // return the first sticky if no non-stickies in the first 6(?) posts
             callback('http://www.reddit.com' + body.data.children[0].data.permalink);
             return;
         }
